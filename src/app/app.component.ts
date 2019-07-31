1b691693c292bff2737aa087a7321527652e338d
Vincent Laude <vincent.laude@atos.net>
2019-07-31 14:47:40 +0200

    bugfix build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,4 +30,8 @@ export class AppComponent implements OnInit {
             this.loading = false;
         }
     }
+
+    isAuthenticated(): boolean {
+        return this.authService.isAuthenticated();
+    }
 }
